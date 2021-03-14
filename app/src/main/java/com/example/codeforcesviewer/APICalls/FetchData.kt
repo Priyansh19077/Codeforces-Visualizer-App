7911ec1d39ec60656decc94968242c9834e07d1d
Priyansh19077 <55681439+Priyansh19077@users.noreply.github.com>
2021-03-14 16:28:18 +0530

    fixed timeout in API by increasing the time out limit using okhttp client

--- app/src/main/java/com/example/codeforcesviewer/APICalls/FetchData.kt
+++ app/src/main/java/com/example/codeforcesviewer/APICalls/FetchData.kt
@@ -2,37 +2,47 @@ package com.example.codeforcesviewer
 
 import com.example.codeforcesviewer.UserData.UserContests
 import com.example.codeforcesviewer.UserData.UserPublicData
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
+
 
 const val url = "https://codeforces.com/api/"
+
 interface ProfileDataFetch {
 
     @GET("user.info")
     fun getUserData(
-            @Query("handles")handle : String,
-            @Query("lang")lang : String = "en") : retrofit2.Call<UserPublicData>
+            @Query("handles") handle: String,
+            @Query("lang") lang: String = "en"): retrofit2.Call<UserPublicData>
 
     @GET("user.ratedList?activeOnly=false&lang=en")
-    fun getAllUsers() : retrofit2.Call<UserPublicData>
+    fun getAllUsers(): retrofit2.Call<UserPublicData>
 
     @GET("user.rating")
     fun getUserRatedContests(
-            @Query("handle")handle : String,
-            @Query("lang")lang : String = "en") : retrofit2.Call<UserContests>
+            @Query("handle") handle: String,
+            @Query("lang") lang: String = "en"): retrofit2.Call<UserContests>
 }
 
 
-
 object FetchData {
-    val instance : ProfileDataFetch
+    var okHttpClient = OkHttpClient().newBuilder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .build()
+    val instance: ProfileDataFetch
+
     init {
         val retrofit = Retrofit.Builder()
-            .baseUrl(url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+                .client(okHttpClient)
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         instance = retrofit.create(ProfileDataFetch::class.java)
     }
 }
